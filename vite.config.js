b1742430278b86726f6feb1e33d24ce53805cae4
Prahlad800 <Prahlad8009872468@gmail.com>
2026-02-24 23:59:55 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/Paradise-Nursery-Shopping-Application/",
+   base: "/",
   plugins: [react()],
 })
